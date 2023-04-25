8a250e688e1f315632abafaa7bb5bbbf10df456a
phuongkon1998 <phuongkon1998@gmail.com>
2023-04-25 13:48:35 +0700

    editprofile

--- src/pages/editProfile/EditProfile.jsx
+++ src/pages/editProfile/EditProfile.jsx
@@ -1,13 +1,100 @@
+import React, { useState } from "react";
+import updateProfile from "./UpdateProfile";
 import "./editprofile.scss";
 
-const EditProf = () =>{
-    return(
-        <div className="edit">
-            Test edit profile
+const EditProfile = () => {
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState(null);
+  const [coverPhoto, setCoverPhoto] = useState(null);
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-        </div>
-    )
+  const handleProfilePicUpload = (event) => {
+    setProfilePic(event.target.files[0]);
+  };
 
+  const handleCoverPhotoUpload = (event) => {
+    setCoverPhoto(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await updateProfile(name, profilePic, coverPhoto, oldPassword, newPassword);
+      setErrorMessage("");
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <div className="user-container">
+      <div className="container">
+      <h1>Edit Profile</h1>
+      {errorMessage && <div>{errorMessage}</div>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <br />
+        <br />
+        <label htmlFor="profilePic">Profile Picture:</label>
+        <input
+          type="file"
+          id="profilePic"
+          accept="image/*"
+          onChange={handleProfilePicUpload}
+        />
+        <br />
+        <br />
+        <label htmlFor="coverPhoto">Cover Photo:</label>
+        <input
+          type="file"
+          id="coverPhoto"
+          accept="image/*"
+          onChange={handleCoverPhotoUpload}
+        />
+        <br />
+        <br />
+        <label htmlFor="oldPassword">Old Password:</label>
+        <input
+          type="password"
+          id="oldPassword"
+          value={oldPassword}
+          onChange={(event) => setOldPassword(event.target.value)}
+          
+        />
+        <br />
+        <br />
+        <label htmlFor="newPassword">New Password:</label>
+        <input
+          type="password"
+          id="newPassword"
+          value={newPassword}
+          onChange={(event) => setNewPassword(event.target.value)}
+        />
+        <br />
+        <br />
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        />
+        <br />
+        <br />
+        <button type="submit">Save Changes</button>
+      </form>
+
+      </div>
+    </div>
+  );
 }
-
-export default EditProf;
+export default EditProfile;
--- src/pages/group/Group.jsx
+++ src/pages/group/Group.jsx
@@ -1,14 +1,76 @@
 import "./group.scss";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts"
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Axios from "axios";
+import Cookies from "js-cookie";
+import { message } from "antd";
+import Share from "../../components/share/Share"
 
-const Group = () =>{
-    return(
-        <div className="group">
-            <div className="grouplist">
-                
+const Group = () => {
+  const token = Cookies.get('token');
+
+  const [dataMember, setDataMember] = useState({});
+
+  const getDataMember = async () => {
+    try {
+      await Axios.get(
+        "https://mystic-network.herokuapp.com/api/member/get-profile",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+        .then((response) => {
+          setDataMember(response.data.member);
+        })
+    } catch (error) {
+      message.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getDataMember();
+  }, []);
+
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src={dataMember.coverImage}
+          alt=""
+          className="cover"
+        />
+        <img
+          src={dataMember.avatar}
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">          
+          </div>
+          <div className="center">
+            <span>{dataMember.username}</span>
+            <div className="info">
+              
             </div>
+            <button>Add Friend</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon /> 
+          </div>
         </div>
-    )
-
-}
+      <Share/>
+      <Posts/>
+      </div>
+    </div>
+  );
+};
 
 export default Group;
